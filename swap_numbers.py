59615b4dbaecfe7ae2a4e110e84882e6ed54131d
batuberk34 <45970453+batuberk34@users.noreply.github.com>
2018-12-18 18:13:53 +0300

    Update swap_numbers.py

--- swap_numbers.py
+++ swap_numbers.py
@@ -1,10 +1,11 @@
+#First input two numeric integers for x(first number) and y (second number)
 x=int(input("Enter a number for x "))
 y=int(input("Enter a number for y "))
     
-
+#Calculations for swapping two numbers without using third varible
 x=x+y
 y=x-y
 x=x-y
-
+#Print out on the screen after swapping process
 print("After swapping\n Now x is= " ,x ,"\n Now y is =" ,y)
  
